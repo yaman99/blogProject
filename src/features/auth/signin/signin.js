a1606aa943f3d7afa7ad475502664123df60e00e
MuhammedDele <muhammed.dele@st.uskudar.edu.tr>
2023-01-04 17:12:50 +0300

    fix sign in

--- src/features/auth/signin/signin.js
+++ src/features/auth/signin/signin.js
@@ -2,6 +2,8 @@ import { AuthService } from "../../../shared/services/authService.js";
 import { NotificationService } from "../../../shared/services/NotificationService.js";
 import "../validation"
 import "../auth.css"
+import { initLogInPage } from "../validation.js";
+initLogInPage();
 // event listners
 document.getElementById("login_btn").addEventListener("click", login, true);
 
@@ -9,10 +11,10 @@ let service = new AuthService();
 let notification = new NotificationService();
 
 function login() {
-  // const email = document.getElementById("email").value;
-  // const password = document.getElementById("password").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
 
-  const user = service.login("yaman@yaman.com", "asd.123");
+  const user = service.login(email, password);
   if (user) {
     window.location.href = "../features/blog-list.html";
   } else {
