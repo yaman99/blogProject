923ac2a8c45fd4f40de843215f3ea894d0c7ca9e
ziadwagdy <zwagdy@gmail.com>
2023-01-04 15:21:37 +0300

    using blog service

--- src/features/blog/blog-list/blog-list.js
+++ src/features/blog/blog-list/blog-list.js
@@ -1,15 +1,18 @@
+import { BlogService } from "../../../shared/services/blogService.js";
 import createBlog from "../BlogPost.js";
 
 // new service blogService
-if (sessionStorage.length == 1 ) { 
+let blogService = new BlogService(); 
+
+let blogs = blogService.getAll(); 
+
+if (blogs.length == 0 ) { 
     const empty = document.createElement("div");
     empty.innerText = "No Blogs yet, add some!";
     document.querySelector(".empty-list").appendChild(empty);
 } else { 
-    
-    for (let i= 1; i < sessionStorage.length; i++) { 
-        let item = JSON.parse(sessionStorage.getItem(Object.keys(sessionStorage)[i]))
-        // use allBlog 
+    for (let i= 1; i < blogs.length; i++) { 
+        let item = blogs[i];
         createBlog(item); 
     }
 }
